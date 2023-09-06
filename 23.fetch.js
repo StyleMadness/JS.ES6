3c917ca1edca5f4fbadd9e47b5041abffa27c79c
StyleMadness <ziksegakiz@gmail.com>
2023-09-06 21:26:56 +0500

    xhr

---
+++ 23.fetch.js
@@ -0,0 +1,34 @@
+const requestUrl = 'https://jsonplaceholder.typicode.com/users'
+
+function sendRequest(method, url, body = null) {
+  const headers = {
+    'Content-type': 'application/json',
+  }
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(body),
+    headers: headers,
+  }).then((response) => {
+    if (response.ok) {
+      return response.json()
+    }
+    return response.json().then((error) => {
+      const err = new Error('W T F')
+      err.data = error
+      throw err
+    })
+  })
+}
+
+// sendRequest('GET', requestUrl)
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err))
+
+const body = {
+  name: 'Style',
+  age: 25,
+}
+
+sendRequest('POST', requestUrl, body)
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err))
